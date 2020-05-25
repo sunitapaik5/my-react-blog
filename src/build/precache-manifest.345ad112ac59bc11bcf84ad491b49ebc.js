062f42c397e905c350cb4708965fa33ef0e4e864
Sunita Paik <sunitapaik5@gmail.com>
2020-05-25 23:36:22 +0530

    initial commit

---
+++ src/build/precache-manifest.345ad112ac59bc11bcf84ad491b49ebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a790a4172d064e37d8d2fad8d874a22f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8102171b49479a4af8d2",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "a1850b1e29f83404fcfa",
+    "url": "/static/js/2.11aa829e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.11aa829e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8102171b49479a4af8d2",
+    "url": "/static/js/main.d5551cf5.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
